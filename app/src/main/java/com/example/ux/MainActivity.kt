7e27809d10d85d397aae41943dadc1fd457ab061
Pablo Rodrigues <pablo.rodrigues2015@outlook.com>
2021-08-02 23:13:10 -0300

    Created RecyclerView

--- app/src/main/java/com/example/ux/MainActivity.kt
+++ app/src/main/java/com/example/ux/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.ux
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private val rvList: RecyclerView by lazy {
+        findViewById<RecyclerView>(R.id.rv_list)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bindViews()
+
+    }
+
+    private fun bindViews(){
+
+
     }
 }

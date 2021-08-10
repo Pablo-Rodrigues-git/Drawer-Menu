6e6b9187a0ee2b77a81290068b760e8de28d1317
Pablo Rodrigues <pablo.rodrigues2015@outlook.com>
2021-08-09 21:28:38 -0300

    Changed import to androidX import, that makes the toolbar works

--- app/src/main/java/com/example/ux/MainActivity.kt
+++ app/src/main/java/com/example/ux/MainActivity.kt
@@ -5,9 +5,15 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -18,10 +24,22 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.drawer_menu)
+
+        initDrawer()
         bindViews()
         updateList()
 
+    }
+
+    private fun initDrawer(){
+        val drawerLayout = findViewById<View>(R.id.drawer_layout) as DrawerLayout
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val toogle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer)
+        drawerLayout.addDrawerListener(toogle)
+        toogle.syncState()
     }
 
     private fun bindViews(){

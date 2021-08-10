cbaa2ee3c49262fd04067d26a66d3a262637b20a
Pablo Rodrigues <pablo.rodrigues2015@outlook.com>
2021-08-09 22:55:32 -0300

    Created ClickItemContactListener.kt to show contact details

--- app/src/main/java/com/example/ux/MainActivity.kt
+++ app/src/main/java/com/example/ux/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.ux
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -16,11 +17,11 @@ import androidx.recyclerview.widget.RecyclerView
 
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ClickItemContactListener {
     private val rvList: RecyclerView by lazy {
         findViewById<RecyclerView>(R.id.rv_list)
     }
-    private val adapter = ContactAdapter()
+    private val adapter = ContactAdapter(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -88,5 +89,10 @@ class MainActivity : AppCompatActivity() {
             }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    override fun clickItemContact(contact: Contact) {
+        val intent = Intent(this,ContactDetail::class.java )
+        startActivity(intent)
     }
 }
